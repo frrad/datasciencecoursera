9293fc62c53b23e1a019c9615af9cefb70dcad19
Frederick Robinson <frederick.robinson@gmail.com>
2015-02-09 21:23:42 -0800

    first few questions from the quiz

---
+++ r-programming/quiz-1.R
@@ -0,0 +1,31 @@
+# Question 4
+x <- 4L
+class(x)
+
+# Question 5
+x <- c(4, TRUE)
+class(x)
+
+# Question 6
+x <- c(1,3, 5)
+y <- c(3, 2, 10)
+rbind(x,y)
+
+# Question 8
+x <- list(2, "a", "b", TRUE)
+x[[1]]
+
+# Question 9
+x <- 1:4
+y <- 2:3
+x + y
+
+# Question 10
+x <- c(3, 5, 1, 10, 12, 6)
+x[x %in% 1:5] <- 0
+x
+
+data = read.csv("hw1_data.csv")
+
+# Question 11
+names(data)
