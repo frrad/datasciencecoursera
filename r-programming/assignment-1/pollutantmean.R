d9ebcb969de49b8807d6fe30e3ca383a11944249
Frederick Robinson <frederick.robinson@gmail.com>
2015-02-16 14:48:35 -0800

    solved pollutant mean

--- r-programming/assignment-1/pollutantmean.R
+++ r-programming/assignment-1/pollutantmean.R
@@ -1,14 +1,30 @@
 pollutantmean <- function(directory, pollutant, id = 1:332) {
-        ## 'directory' is a character vector of length 1 indicating
-        ## the location of the CSV files
+    ## 'directory' is a character vector of length 1 indicating
+    ## the location of the CSV files
 
-        ## 'pollutant' is a character vector of length 1 indicating
-        ## the name of the pollutant for which we will calculate the
-        ## mean; either "sulfate" or "nitrate".
+    ## 'pollutant' is a character vector of length 1 indicating
+    ## the name of the pollutant for which we will calculate the
+    ## mean; either "sulfate" or "nitrate".
 
-        ## 'id' is an integer vector indicating the monitor ID numbers
-        ## to be used
+    ## 'id' is an integer vector indicating the monitor ID numbers
+    ## to be used
 
-        ## Return the mean of the pollutant across all monitors list
-        ## in the 'id' vector (ignoring NA values)
+    ## Return the mean of the pollutant across all monitors list
+    ## in the 'id' vector (ignoring NA values)
+    pool = c()
+
+    for(sensor in id) {
+        data = read.csv(sprintf("%s/%03d.csv", directory, sensor))
+        polluted = data[!is.na(data[pollutant]), pollutant]
+
+        pool <- c(polluted, pool)
+    }
+
+    mean(pool)
+
+
 }
+
+## pollutantmean("specdata", "sulfate", 1:10)
+## pollutantmean("specdata", "nitrate", 70:72)
+## pollutantmean("specdata", "nitrate", 23)
